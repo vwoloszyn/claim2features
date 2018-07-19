43bee1500033aa681f094d8e214f17c05123d8c8
eduardogc8 <eduardogcortes8@gmail.com>
2018-07-19 16:03:48 -0300

    Annotating labels - to be continued

--- main.py
+++ main.py
@@ -6,10 +6,27 @@ import requests
 
 sites = [
     "https://www.snopes.com/fact-check/hillary-clinton-smash-phone-hammer/",
+    "https://africacheck.org/reports/eskom-and-the-viral-infographic-do-the-numbers-add-up/",
+    "http://checkyourfact.com/2018/07/09/fact-check-nfl-ratings-20-percent/",
+    "https://www.factcheck.org/2018/07/trumps-false-claims-at-nato/",
+    "https://theferret.scot/scotland-oil-abu-dhabi-dubai/",
+    "https://fullfact.org/europe/uk-one-biggest-contributors-eu-budget/?utm_source=homepage&utm_medium=main_story",
+    "https://hoax-alert.leadstories.com/3469573-fake-news-florida-babysitter-tied-crying-one-month-old-baby-to-ceiling-fan-for-26-hours.html",
+    "https://pesacheck.org/have-58-of-ugandas-roads-been-built-using-only-government-money-e2156f2aa41a",
+    "http://www.politifact.com/personalities/sean-hannity/",
+    "https://theconversation.com/factcheck-is-australias-population-the-highest-growing-in-the-world-96523",
+    "https://www.washingtonpost.com/news/fact-checker/wp/2018/07/18/phil-bredesens-claim-that-tennessees-meth-problem-was-cut-in-half/?utm_term=.f3688d506f6c",
+    "https://apnews.com/tag/APFactCheck",
+    "https://climatefeedback.org/evaluation/usa-today-paleoclimate-study-mostly-accurate-lacks-clarity-doyle-rice/",
+    "https://www.factcheckni.org/facts/100000-adults-northern-ireland-no-home/",
 ]
 
 ignore_html_tags = ['script']
+text_tags = ['p', 'strong', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'li', 'b', 'i', 'u', 'title']
 
+
+def treat_text(text):
+    return text.replace('\n', '').strip()
 
 def get_url(url):
     headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
@@ -28,16 +45,16 @@ def walk_html(element, data, site, level):
     features['url'] = site
     features['level'] = level
     features['label'] = 'None'
-    if len(list(element))==0:
-        features['text'] = element.text
+    if element.name in text_tags:
+        features['text'] = treat_text(element.text)
     else:
-        features['text'] = 'NOT LEAF'
+        features['text'] = 'NOT TEXT'
 
     data.append(features)
     for e_child in element:
         if (type(e_child) == bs4.element.NavigableString or 
-            type(e_child) == bs4.element.Comment or 
-            type(e_child) == bs4.element.Doctype or 
+            type(e_child) == bs4.element.Comment or
+            type(e_child) == bs4.element.Doctype or
             type(e_child) == str or
             e_child.name in ignore_html_tags):
             continue
@@ -50,6 +67,7 @@ def extract_features(html, data, site):
 if __name__ == "__main__":
     data=[]
     for url in sites:
+        print('url:', url)
         html = get_url(url)
         extract_features(html, data, url)
 
